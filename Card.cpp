bdb62addf5dd74763e6dd2b225f509ad44287680
khalifatamer <tamerkhalifa@campus.technion.ac.il>
2022-12-03 22:04:06 +0200

    version 1

--- Card.cpp
+++ Card.cpp
@@ -1,3 +1,62 @@
-//
-// Created by Tamer Khalifa on 29/11/2022.
-//
+#include "Card.h"
+
+Card::Card(CardType type, const CardStats &stats)
+{
+    m_effect = type;
+    m_stats = stats;
+}
+
+void Card::applyEncounter(Player& player) const
+{
+    if(m_effect == CardType::Battle)
+    {
+        if(player.getAttackStrength() >= m_stats.force){
+            player.levelUp();
+            player.addCoins(m_stats.loot);
+            printBattleResult(true);
+        }
+        else
+        {
+            player.damage(m_stats.hpLossOnDefeat);
+            printBattleResult(false);
+        }
+    }
+
+    if(m_effect == CardType::Buff)
+    {
+        player.buff(m_stats.buff);
+        return;
+    }
+
+    if(m_effect == CardType::Heal)
+    {
+        player.heal(m_stats.heal);
+        return;
+    }
+
+    if(m_effect == CardType::Treasure)
+    {
+        player.addCoins(m_stats.loot);
+        return;
+    }
+
+
+}
+
+void Card::printInfo() const
+{
+    switch (m_effect) {
+        case CardType::Battle:
+            printBattleCardInfo(m_stats);
+            break;
+        case CardType::Buff:
+            printBuffCardInfo(m_stats);
+            break;
+        case CardType::Heal:
+            printHealCardInfo(m_stats);
+            break;
+        case CardType::Treasure:
+            printTreasureCardInfo(m_stats);
+            break;
+    }
+}
